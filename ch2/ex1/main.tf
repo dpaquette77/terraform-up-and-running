dc61ebffd674fc4641fba120b0a146a49123a9f8
dpaquette <dpaquette@ubity.com>
2020-04-06 06:53:13 -0400

    add ALB

--- ch2/ex1/main.tf
+++ ch2/ex1/main.tf
@@ -21,8 +21,8 @@ variable "http_port" {
 }
 
 output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "The public ip of the instance where the webserver is running"
+  value = aws_lb.example.dns_name
+  description = "DNS name of the load balancer"
 }
 
 resource "aws_launch_configuration" "example" {
@@ -52,7 +52,8 @@ resource "aws_autoscaling_group" "example" {
         propagate_at_launch = true
     }
 
-    
+    target_group_arns = [aws_lb_target_group.mytarget-group.arn]
+    health_check_type = "ELB"
 }
 
 data "aws_vpc" "default" {
@@ -62,3 +63,79 @@ data "aws_vpc" "default" {
 data "aws_subnet_ids" "default" {
     vpc_id = data.aws_vpc.default.id
 }
+
+
+resource "aws_lb" "example" {
+    name = "terraform-lb-example"
+    load_balancer_type = "application"
+    subnets = data.aws_subnet_ids.default.ids
+    security_groups = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_listener" "example" {
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP"
+
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+            content_type = "text/plain"
+            message_body = "404: page not found"
+            status_code = 404
+        }
+    }
+}
+
+resource "aws_security_group" "alb_sg" {
+    name = "terraform-alb-sg"
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
+
+resource "aws_lb_target_group" "mytarget-group" {
+    name = "terraform-example-target-group"
+    port = var.http_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+    
+    health_check {
+        path = "/"
+        protocol = "HTTP"
+        matcher = "200"
+        interval = 15
+        timeout = 3
+        healthy_threshold = 2
+        unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener_rule" "lblistenerrule" {
+    listener_arn = aws_lb_listener.example.arn
+    priority = 100
+    condition {
+        field = "path-pattern"
+        values = ["*"]
+    }
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.mytarget-group.arn
+    }
+  
+}
+
+
