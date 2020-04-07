00b242dadbc0e9fe40fe76b22aa3fa4f4c6d52bd
dpaquette <dpaquette@ubity.com>
2020-04-07 07:57:18 -0400

    move dynamodb to correct location

--- ch3/main.tf
+++ ch3/main.tf
@@ -21,16 +21,16 @@ resource "aws_s3_bucket" "terraform_state" {
             }
         }
     }
+}
 
-    # dynamoDB table for lock management
-    resource "aws_dynamodb_table" "terraform_locks" {
-        name = "terraform-up-and-running-locks"
-        billing_mode = "PAY_PER_REQUEST"
-        hash_key = "LockID"
+# dynamoDB table for lock management
+resource "aws_dynamodb_table" "terraform_locks" {
+    name = "terraform-up-and-running-locks"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
 
-        attribute {
-            name = "LockID"
-            type = "S"
-        }
+    attribute {
+        name = "LockID"
+        type = "S"
     }
 }
