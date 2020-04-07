2bd5433a33638fcfe48c0660dc8d1cb75d2f1a91
dpaquette <dpaquette@ubity.com>
2020-04-07 07:53:40 -0400

    create dynamodb table for terraform lock management

--- ch3/main.tf
+++ ch3/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
     region = "us-east-2"
 }
 
+# s3 bucket to store terraform state file
 resource "aws_s3_bucket" "terraform_state" {
     bucket = "dpaquette-terraform-up-and-running-state"
 
@@ -18,6 +19,18 @@ resource "aws_s3_bucket" "terraform_state" {
             apply_server_side_encryption_by_default {
                 sse_algorithm = "AES256"
             }
+        }
+    }
+
+    # dynamoDB table for lock management
+    resource "aws_dynamodb_table" "terraform_locks" {
+        name = "terraform-up-and-running-locks"
+        billing_mode = "PAY_PER_REQUEST"
+        hash_key = "LockID"
+
+        attribute {
+            name = "LockID"
+            type = "S"
         }
     }
 }
