d8d1bcb642261f6a6f9a3a8efc413fe590747ef0
dpaquette <dpaquette@ubity.com>
2020-04-10 09:40:43 -0400

    add ssm parameter store password

--- ch3/filelayout-example/stage/data-stores/mysql/main.tf
+++ ch3/filelayout-example/stage/data-stores/mysql/main.tf
@@ -1,21 +1,26 @@
 terraform {
-    backend = "s3"
-    bucket = "dpaquette-terraform-up-and-running-state"
-    key = "stage/services/data-stores/mysql/terraform.tfstate"
-    region = "us-east-2"
-    dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
+    backend "s3" {
+        bucket = "dpaquette-terraform-up-and-running-state"
+        key = "stage/services/data-stores/mysql/terraform.tfstate"
+        region = "us-east-2"
+        dynamodb_table = "terraform-up-and-running-locks"
+        encrypt = true
+    }
 }
 
 provider "aws" {
     region = "us-east-2"
 }
-resources "aws_db_instance" "example" {
+resource "aws_db_instance" "example" {
     identifier_prefix = "terraform-up-and-running"
     engine = "mysql"
     allocated_storage = 10
     instance_class = "db.t2.micro"
     name = "example_database"
     username = "admin"
-    password = "???"
+    password = data.aws_ssm_parameter.db_password.value
+}
+
+data "aws_ssm_parameter" "db_password" {
+    name = "db_password"
 }
