38aa12966f12afc1b161d8b122552ddf09c33750
dpaquette <dpaquette@ubity.com>
2020-04-17 10:45:28 -0400

    add output of all names in uppercase

--- ch5/for_each_example/outputs.tf
+++ ch5/for_each_example/outputs.tf
@@ -7,3 +7,8 @@ output "all_arns" {
     description = "all users arns"
     value = values(aws_iam_user.users)[*].arn
 }
+
+output "uppercase_names" {
+    description = "all names in uppercase"
+    value = [for name in var.usernames : upper(name)]
+}
